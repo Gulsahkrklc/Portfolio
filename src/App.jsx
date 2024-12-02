02577f43f04cc3dee588b84945506a805a23104e
Gulsahkrklc <gulsah.krklc44@gmail.com>
2024-12-02 16:57:41 +0300

    İlk commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl underline text-red-500">
+      Hello world!
+    </h1>
+        
+        
+        
+      
+    </>
+  )
+}
+
+export default App
